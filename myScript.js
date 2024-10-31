99612e11ffce39a0a6d63e9b15bfc14668c78ec6
vitareali <khvedelidzenick35@gmail.com>
2024-10-31 16:51:17 +0400

    Create myScript.js

---
+++ myScript.js
@@ -0,0 +1,42 @@
+const navButtonOpen = document.getElementById("navButtonOpen");
+
+function openSideNav() {
+    if (document.getElementById("mySidenav").style.width === "210px") {
+    document.getElementById("mySidenav").style.width = "0";
+    } else {
+    document.getElementById("mySidenav").style.width = "210px";
+    }
+}
+/*
+function toggleMode() {
+    var darkMode = document.body;
+    darkMode.classList.toggle("dark-mode");
+}*/
+function toggleMode() {
+    const containerBody = document.body;
+    let theme = localStorage.getItem("data-theme");
+
+if (theme === "light"){ 
+    containerBody.setAttribute("data-theme", "dark");
+    localStorage.setItem("data-theme", "dark");
+    document.getElementById('btn-toggle').textContent = "Dark Mode";
+    } else {
+    containerBody.setAttribute("data-theme","light");
+    localStorage.setItem("data-theme", "light");
+    document.getElementById('btn-toggle').textContent = "Light Mode";
+    }
+}
+
+function loadTheme(){
+    let containerBody = document.body;
+    let dataTheme = containerBody.getAttribute("data-theme");
+    let theme = localStorage.getItem("data-theme");
+
+if (theme === "dark"){ 
+    containerBody.setAttribute("data-theme", "dark");
+    document.getElementById('btn-toggle').textContent = "Dark Mode";
+    } else {
+    containerBody.setAttribute("data-theme","light");
+    document.getElementById('btn-toggle').textContent = "Light Mode";
+    }
+}
